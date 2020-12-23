cdd75a2d597070ba7551ae05f70601403d26a580
tramnguyen1602 <75562505+tramnguyen1602@users.noreply.github.com>
2020-12-23 18:14:09 +0700

    Update showcart.js

--- JS/showcart.js
+++ JS/showcart.js
@@ -4,7 +4,7 @@ window.onload=   function HienThiList(){
    
     if(ListItemCart.length==0)
     {
-        html=' <div class="giohangtrong" <div class="trong"> GIỎ HÀNG ĐANG TRỐNG BẠN HÃY MUA THÊM SẢN PHẨM!</div> </div>';
+        html=' <div class="giohangtrong" <div class="trong"> GIỎ HÀNG ĐANG TRỐNG, BẠN HÃY MUA THÊM SẢN PHẨM!</div> </div>';
         // ẩn button đặt hàng
 
         document.getElementById('btndathang').style.display='none'
